68895f83b8942237131f9bf13b09a28f12c53739
crypticcu <crypticcu@protonmail.com>
2021-01-14 23:58:07 -0500

    Update and rename parse.h to eval.h

--- eval.h
+++ eval.h
@@ -5,7 +5,6 @@
 
 #ifdef __GNU_LIBRARY__
 #include <err.h>
-
 #define PROG_NAME\
 	program_invocation_name[1] == '/' ?\
 	program_invocation_name + 2 :\
@@ -13,7 +12,7 @@
 
 extern char *program_invocation_name;
 #else
-#define PROG_NAME	"parsr"
+#define PROG_NAME	"eval"
 #endif /* #ifdef __GNU_LIBRARY__ */
 
 /* Initializes left- and right-hand values in evaluate() */
@@ -25,7 +24,7 @@ extern char *program_invocation_name;
 
 /* Initializes new expression string in evaluate() */
 #define INIT_EXPR()\
-	if ((result_str = dtos(result, LDBL_DIG)) == NULL) {\
+	if ((result_str = dtos(result, DBL_DIG)) == NULL) {\
 		free(expr);\
 		fail("Internal error (evaluate.dtos)");\
 	}\
@@ -34,42 +33,45 @@ extern char *program_invocation_name;
 		fail("Internal error (evaluate.pushsub)");\
 	}
 
-#define INT_FAIL	INT_MAX	// Passed by [type]-returning functions on failure
+#define INT_FAIL	INT_MAX		// Passed by [type]-returning functions on failure
 #define DBL_FAIL	DBL_MAX
-#define CHK_PASS	-1		// Used by chk_parenth() and chk_syntax(); Indicates valid syntax
-#define OBS_R		1		// Used by fobst(); Indicates obstruction in operation
+#define DBL_OVER	FLT_EPSILON	// Passed by [type]-returning functions on overflow
+#define STR_OVER	"..."
+#define CHK_PASS	-1			// Used by chk_parenth() and chk_syntax(); Indicates valid syntax
+#define OBS_R		1			// Used by fobst(); Indicates obstruction in operation
 #define OBS_L		2
+//#define DEBUG					// Define to turn on debugging mode
 
-static bool DEBUG = false;
+extern bool CMD_LINE;
 static const char *VAL_CHRS = "+-!^*/%.()\n1234567890",
-				  *OPERS_A = "+-!^*/%",	// All
-		  		  *OPERS_S = "^*/%",	// Single
-		  		  *OPERS_D = "+-!";		// Double
+		  		  *OPERS = "+-!^*/%",
+		  		  *DBLS = "+-!";		// Double operators
 
 /* Prints error message and exits program */
 extern void fail(const char *_desc);
 
-/* Prints string with character at given position highlighted red */
-extern void printh(const char *_s, unsigned _hpos);
+/* Prints string with character at given position underlined */
+extern void printu(const char *_s, size_t _hpos);
 
 /* Returns string representation of double
  * Returns NULL on failure
+ * Returns OVERFLOW on overflow
  * Resulting string must be freed */
-extern char *dtos(double _x, unsigned _sig);
+extern char *dtos(double _x, size_t _sig);
 
 /* Returns new, allocated substring spanning the given elements
  * Returns NULL on failure */
-extern char *popsub(const char *_s, unsigned _low, unsigned _high);
+extern char *popsub(const char *_s, size_t _low, size_t _high);
 
 /* Returns new, allocated string where substring replaces given elements in old string
  * Frees given string and substring while returning a newly allocated one
  * Returns NULL on failure */
-extern char *pushsub(char *_s, char *_sub, unsigned _low, unsigned _high);
+extern char *pushsub(char *_s, char *_sub, size_t _low, size_t _high);
 
 /* Evaluates mathemetical expression starting from given index position
  * Returns string representation of result depending on index
  * Returns NULL on failure */
-extern char *simplify(const char *_expr, unsigned _from);
+extern char *simplify(const char *_expr, size_t _from);
 
 /* Returns true if floating-point numbers are equal */
 extern bool isequal(double _x, double _y);
@@ -81,17 +83,17 @@ extern bool isin(const char _x, const char *_y);
 extern bool isnumer(char _c);
 
 /* Determines whether a parenthesis indicates multiplication */
-extern bool toast(const char *_expr, unsigned _parpos);
+extern bool toast(const char *_expr, size_t _parpos);
 
 /* Returns OBS_R or OBS_L depending on type of obstruction(s)
  * If none are found, returns 0 */
-extern unsigned isclr(const char *_expr, unsigned _operpos, unsigned _low, unsigned _high);
+extern size_t isclr(const char *_expr, size_t _operpos, size_t _low, size_t _high);
 
 /* Get digit at given place */
-extern unsigned getdigit(double _x, int _place);
+extern size_t getdigit(double _x, int _place);
 
 /* Returns number of whole places */
-extern unsigned nplaces(double _x);
+extern size_t nplaces(double _x);
 
 /* Returns index position of first invalid parenthesis of expression
  * Returns CHK_PASS if no invalid parentheses are found */
@@ -103,12 +105,12 @@ extern int chk_syntax(const char *_expr);
 
 /* Returns left- or right-hand limit of range of operation at given position in expression
  * Returns INT_FAIL if invalid direction or operand is missing */
-extern int getlim(char *_expr, unsigned _operpos, char _dir);
+extern int getlim(char *_expr, size_t _operpos, char _dir);
 
 /* Returns number of indices operator is from closest obstruction in operation
  * If none are found, returns 0
  * Returns INT_FAIL on failure */
-extern unsigned fobst(const char *_expr, unsigned _operpos, unsigned _llim, unsigned _rlim);
+extern size_t fobst(const char *_expr, size_t _operpos, size_t _llim, size_t _rlim);
 
 /* Evaluates mathematical expression from index position 0
  * Ignores parentheses and syntax errors
@@ -117,10 +119,10 @@ extern double evaluate(const char *_expr);
 
 /* Returns left- or right-hand value of the operand at given position in the expression
  * Returns DBL_FAIL on failure */
-extern double getval(char *_expr, unsigned _operpos, char _dir);
+extern double getval(char *_expr, size_t _operpos, char _dir);
 
 /* Returns double representation of string
- * Stops program on failure */
+ * Returns DBL_FAIL on overflow */
 extern double stod(const char *_s);
 
 #endif /* #ifndef PARSE_H */
